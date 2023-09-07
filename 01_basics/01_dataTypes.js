8dfe16ebb8ebdbbf06eb215b26efc0bc0d925e6b
Syed Majid Bukhari <32079437+syedmaji@users.noreply.github.com>
2023-09-07 20:24:53 +0000

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all javascript code as a newer version 
+
+// alert (2+2) 
+// ReferenceError: alert is not defined, we cannot use alert in the nodejs, it is used in the browser only 
+
+
+let name= "majid"
+let age = 43
+let  isLoggedIn= false
+
+
+console.log (`Mr.${name} , Your age is ${age} and your Login value is ${isLoggedIn}`)
+
+/*
+
+PREMITIVE DATA TYPES
+====================
+
+ //number= 2 to the power of 53
+ //bigint
+ //String= ""
+ //boolean= true /false
+ //null= standlone value 
+ //undefine
+ //Symbol = unique value
+ */
+
+ /*
+
+NON-PREMITIVE DATA TYPES
+====================
+ ARRAYS
+ OBJECT
+ */
